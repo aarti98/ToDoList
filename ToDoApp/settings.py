0f2b5a1f3185f93a98753125eb2c8c46f0ac31a7
aa <aj@gmail.com>
2019-04-23 00:57:02 +0530

    Updated settings.py

--- ToDoApp/settings.py
+++ ToDoApp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'rq_tjxli@u$zwe&qyw+)8%)#y-+k+hovu1-$j8-em_!ffi*g3f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['http://maketime.in']
 
